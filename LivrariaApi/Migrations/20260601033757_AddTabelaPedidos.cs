606274e896ff9e880734043718c459c376716b36
Lucas Santos <lucas.saantos987@gmail.com>
2026-06-10 21:30:07 -0300

    Dia 10-11 — Testes unitários com XUnit, [Fact] e [Theory] implementados

---
+++ LivrariaApi/Migrations/20260601033757_AddTabelaPedidos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LivrariaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTabelaPedidos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PedidoId",
+                table: "Livros",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataPedido = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    NomeCliente = table.Column<string>(type: "TEXT", nullable: false),
+                    TelefoneCliente = table.Column<string>(type: "TEXT", nullable: false),
+                    StatusPedido = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_PedidoId",
+                table: "Livros",
+                column: "PedidoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Livros_Pedidos_PedidoId",
+                table: "Livros",
+                column: "PedidoId",
+                principalTable: "Pedidos",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Livros_Pedidos_PedidoId",
+                table: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Livros_PedidoId",
+                table: "Livros");
+
+            migrationBuilder.DropColumn(
+                name: "PedidoId",
+                table: "Livros");
+        }
+    }
+}
---
+++ LivrariaTests/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using LivrariaCore.Models;
+
+namespace LivrariaTests
+{
+    public class UnitTest1
+    {
+
+        [Theory]
+        [InlineData(50, 65)] //preco 50 -> espera 65
+        [InlineData(100, 115)] //preco 100 -> preco 115
+        [InlineData(0, 15)] //preco 0 -> espera 15
+        public void LivroFisico_CalculoPrecoUnitario_DeveAplicarFreteDe15Reais(double preco, double esperado)
+        {
+            //Arrange - prepara o cenário
+            var livro = new LivroFisico(1, "Harry Potter", preco, "J.K Rowling", "DURA", 4);
+
+
+            //Act - Executa o que quer testar
+            var resultado = livro.CalculoPrecoUnitario();
+
+            //Assert - Verifica se o resultado é esperado
+            Assert.Equal(esperado, resultado);
+
+        }
+
+        [Fact]
+        public void LivroDigital_CalculoPrecoUnitario_DeveAplicarDescontoDe15Porcento() {
+
+            var livroDigital = new LivroDigital(2, "Five Nights at Freddy's The Fourth Closet", 100, "Scott Cawthon", "PNG", 2);
+
+            var resultadoDigital = livroDigital.CalculoPrecoUnitario();
+
+            Assert.Equal(85, resultadoDigital);
+        
+        }
+
+        [Fact]
+        public void LivroFisico_DeveRetornarExceçãoCasoPrecoNegativo()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                  new LivroFisico(1, "LivroNegativado", -10, "Autor", "DURA", 1));
+        }
+
+        [Fact]
+        public void Pedido_CalcularTotal_DeveRetornarPrecoCorretoEntreCalculoDoisObjetos()
+        {
+
+            var pedido = new Pedido();
+
+            var fisico = new LivroFisico(4, "Livro Físico", 50, "Autor", "SIMPLES", 1);
+            var digital = new LivroDigital(3, "Livro Digital", 100, "Autor", "PDF", 1);
+
+            pedido.Livros.Add(fisico) ;
+            pedido.Livros.Add(digital);
+
+            var resultadoPedido = pedido.CalcularTotal();
+
+            Assert.Equal(150, resultadoPedido);
+
+        }
+    }
+}
